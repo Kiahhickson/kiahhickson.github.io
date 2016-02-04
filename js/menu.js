a59be346b77a8d0454ecd50f8e8ce381cde69c23
Kiah Hickson <kiah@kiahhickson.com.au>
2016-02-04 18:18:51 +1100

    Added skills section, updated menu and css styling

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 	$(document).ready(function() {
 	$(".burger, .menu").click(function(){
+    $(window).scrollTop(0); // Move to the top of the document
 		$(this).parent().find(".hide").slideToggle();
 	});
 });
