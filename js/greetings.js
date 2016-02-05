6a44192360c827d6abcfb8233d13aa3295a11801
Kiah Hickson <kiah@kiahhickson.com.au>
2016-02-05 14:46:14 +1100

    Changed layout etc

---
+++ js/greetings.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    var quotes = ['Hello', 'Shalom', 'Ni Hao', 'Namaste', 'Hallo'];
+
+    var $quotes = $(".quotes");
+    var quoteIndex = 0;
+    
+    function showNextQuote() {
+        var q = quotes[quoteIndex];
+        if (q === undefined) {
+           quoteIndex = 0;
+           q = quotes[0];
+        }
+
+        $quotes.fadeOut(function () {
+           $quotes.text(q).fadeIn();
+        });
+        
+        ++quoteIndex;
+        setTimeout(showNextQuote, 3000);
+
+    }
+    
+    showNextQuote();
+    
+});
